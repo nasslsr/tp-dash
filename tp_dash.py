c8f7bb975afb2f194d0d9d2c86240cdcb6a2586e
nasslsr <117176437+nasslsr@users.noreply.github.com>
2023-04-19 23:54:57 +0200

    Update tp_dash.py

--- tp_dash.py
+++ tp_dash.py
@@ -163,6 +163,18 @@ app.layout = html.Div(children=[
             'background-color': 'black',
             'padding': '10px',
             'margin-top': '50px'}),
+    ]),
+    html.Div(children=[
+        html.H1("Les stations d'Ile de France et ses lignes", style={'color': 'green'}),
+        dcc.Graph(id="map-graph", figure=px.scatter_mapbox(
+            df_idf,
+            lat='latitude',
+            lon='longitude',
+            hover_name='nom_long',
+            zoom=8,
+            color_discrete_map=couleurs,
+            color='ligne'
+        ).update_layout(mapbox_style='open-street-map', height=500))
     ])
 
 ])
@@ -215,6 +227,8 @@ def update_bar_chart(ligne):
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
 
 
 
